3a4b1885d966f7b00e289c08f9e477a4de99829d
Virgil Dupras <hsoft@hardcoded.net>
2016-06-18 21:25:03 -0400

    Add href to reddit links output

--- src/reddit.rs
+++ src/reddit.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 use kuchiki;
 use kuchiki::traits::*;
 use liquid;
@@ -5,18 +7,36 @@ use liquid::{Renderable, Context, Value};
 
 pub fn fix(html: &str) -> String {
     let doc = kuchiki::parse_html().one(html);
-    let mut titles: Vec<Value> = Vec::new();
+    let mut links: Vec<Value> = Vec::new();
     let matches = doc.select("div.thing a.title").unwrap();
 
     for css_match in matches {
+        let mut link: HashMap<String, Value> = HashMap::new();
         let node = css_match.as_node();
         let text_node = node.first_child().unwrap();
-        let text = Value::Str(text_node.as_text().unwrap().borrow().clone());
-        titles.push(text);
+        let title = text_node.as_text().unwrap().borrow().clone();
+        link.insert("title".to_owned(), Value::Str(title));
+        let node_attrs = node.as_element().unwrap().attributes.borrow();
+        let href = node_attrs.get("href").unwrap();
+        link.insert("href".to_owned(), Value::Str(href.to_owned()));
+        links.push(Value::Object(link));
     }
 
-    let template = liquid::parse("<html><body><ul>{% for title in titles %}<li>{{ title }}</li>{% endfor %}</ul></body></html>", Default::default()).unwrap();
+    let template = liquid::parse(
+        "
+<html>
+    <body>
+        <ul>
+        {% for link in links %}
+            <li><a href=\"{{ link.href }}\">{{ link.title }}</a></li>
+        {% endfor %}
+        </ul>
+    </body>
+</html>
+",
+        Default::default()
+    ).unwrap();
     let mut context = Context::new();
-    context.set_val("titles", Value::Array(titles));
+    context.set_val("links", Value::Array(links));
     template.render(&mut context).unwrap().unwrap()
 }
