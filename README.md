d20e127da5c1ce60b6e5b75d5db6c902cd2b66da
Virgil Dupras <hsoft@hardcoded.net>
2016-06-20 21:31:59 -0400

    Add cookie proxying
    To be able to do things like log in a website, we intercept cookies from
    our responses, store them in memory and re-send them to the website the
    next time we hit it.
    
    For now, the implementation is a bit messy because it involves fancy
    inter-thread memory sharing and it's the first time I seriously dig in
    that stuff. So for now, tons of `unwrap()` and `clone()` all around!
    
    That's what I like about Rust: dangers and innefficiencies are always in
    your face in the form of `unwrap()` and `clone()`. In another language,
    code elegance could fool you into thinking it was solid code.

--- README.md
+++ README.md
@@ -19,7 +19,9 @@ semantics that is easily processed by a ligtweight browser.
 
 ## Status
 
-There's nothing here yet, it's just me fiddling with [hyper][hyper].
+Very early. Will most likely panic all the time. Websites for which there's a filter:
+
+* [Reddit](www.reddit.com): List of links for all pages that have some.
 
 ## How it works
 
@@ -39,8 +41,11 @@ But then, I don't know yet. I want to try.
 After you've compiled with cargo, simply run `webypass` without argument. It's going to listen
 on `127.0.0.1:8080`.
 
-Then, in your browser, open `http://localhost:8080/?url=http://www.example.com`. You're going
-to get your web page returned. For now, it's returned as is. Pretty useless hey?
+Then, in your browser, open `http://localhost:8080/?url=https://www.reddit.com`. You're going
+to get your web page returned. If it's a supported website, the content is going to be mangled
+so that it's actually usable in a text-based browser.
+
+If it's not supported, the content will be returned as-is.
 
 Any other request type will yield a `404`.
 
@@ -52,6 +57,23 @@ of SSL is to ensure that there's no eavesdropping and/or content alteration. Unf
 precisely what this application does.
 
 So no, it's got to be that way.
+
+### Cookies
+
+Because webypass' URL is `http://localhost`, it's not going to receive proper cookies from your
+browser. However, when the website responds, it's going to respond with proper `Set-Cookie`. For
+this reason, webypass **intercepts** all cookies, keep them in memory, and re-send them with your
+proxied request the next time to hit that website.
+
+For now, that's the only practical way I've found to do things like being logged in a website.
+
+For now, the cookie cache is only in memory. It's never written to persistent storage.
+
+### Privacy
+
+Webypass has access to all the contents of your HTTP requests and it stores all your cookies. For
+this reason, you should only run it **locally**. You don't want to share your instance of webypass
+with anyone else.
 
 [w3m]: http://w3m.sourceforge.net/
 [midori]: http://midori-browser.org/
--- src/main.rs
+++ src/main.rs
@@ -7,72 +7,138 @@ mod reddit;
 
 use std::io::{Read, Write};
 use std::str;
+use std::collections::HashMap;
+use std::thread;
+use std::sync::{Arc, Mutex};
+use std::sync::mpsc::{Sender, SyncSender, Receiver, channel, sync_channel};
 
 use hyper::Url;
 use hyper::uri:: RequestUri;
-use hyper::header::{Headers, AcceptEncoding};
+use hyper::header::{Headers, AcceptEncoding, CookiePair, SetCookie, Cookie};
 use hyper::client::Client;
-use hyper::server::{Server, Request, Response};
+use hyper::server::{Server, Request, Response, Handler};
 
 use fixdom::fixdom;
 
-fn proxy(req: Request, mut res: Response, target_url: &str) {
-    let client = Client::new();
-    let mut req_headers = req.headers.clone();
-    req_headers.remove::<AcceptEncoding>();
-    println!("debug {:?}", &req_headers);
-    let mut proxied_res = client.request(req.method, target_url).headers(req_headers).send().unwrap();
-    let mut buffer = Vec::new();
-    let _ = proxied_res.read_to_end(&mut buffer);
-    {
-        let h: &mut Headers = res.headers_mut();
-        *h = proxied_res.headers.clone();
-        println!("debug {:?}", &h);
-    }
-    let mut res = res.start().unwrap();
-    println!("debug {}", buffer.len());
-    match fixdom(&Url::parse(target_url).unwrap(), str::from_utf8(&buffer).unwrap()) {
-        Some(fixed) => res.write_all(&fixed.as_bytes()).unwrap(),
-        None => res.write_all(&buffer).unwrap(),
-    };
-}
 
-fn get_url_in_query(url: &Url) -> Option<String> {
+fn get_url_in_query(url: &Url) -> Option<Url> {
     let find_res = url.query_pairs().find(|&(ref k, _)| k == "url");
     if let Some((_, qurl)) = find_res {
-        Some(qurl.as_ref().to_string())
+        let url_str = qurl.as_ref();
+        Some(Url::parse(url_str).unwrap())
     } else {
         None
     }
 }
 
-fn hello(req: Request, mut res: Response) {
-    let uri = req.uri.clone();
-    match uri {
-        RequestUri::AbsoluteUri(url) => {
-            println!("URI: {}", url);
-            if let Some(target_url) = get_url_in_query(&url) {
-                proxy(req, res, &target_url);
-                return;
+struct WebypassHandler {
+    cookie_sender: Mutex<Sender<(Url, Vec<CookiePair>)>>,
+    cookie_requester: Mutex<SyncSender<Url>>,
+    cookie_receiver: Mutex<Receiver<Option<Vec<CookiePair>>>>,
+}
+
+impl Handler for WebypassHandler {
+
+    fn handle(&self, req: Request, mut res: Response) {
+        let uri = req.uri.clone();
+        match uri {
+            RequestUri::AbsoluteUri(url) => {
+                println!("URI: {}", url);
+                if let Some(target_url) = get_url_in_query(&url) {
+                    self.proxy(req, res, &target_url);
+                    return;
+                }
+            },
+            RequestUri::AbsolutePath(path) => {
+                println!("Path {}", path);
+                let url = Url::parse("http://localhost").unwrap().join(&path).unwrap();
+                if let Some(target_url) = get_url_in_query(&url) {
+                    self.proxy(req, res, &target_url);
+                    return;
+                }
+            },
+            _ => {
+                println!("Unsupported URI type");
             }
-        },
-        RequestUri::AbsolutePath(path) => {
-            println!("Path {}", path);
-            let url = Url::parse("http://localhost").unwrap().join(&path).unwrap();
-            if let Some(target_url) = get_url_in_query(&url) {
-                proxy(req, res, &target_url);
-                return;
-            }
-        },
-        _ => {
-            println!("Unsupported URI type");
         }
+        *res.status_mut() = hyper::NotFound;
+        res.send(b"Not Found").unwrap();
     }
-    *res.status_mut() = hyper::NotFound;
-    res.send(b"Not Found").unwrap();
+
+}
+
+impl WebypassHandler {
+    fn proxy(&self, req: Request, mut res: Response, target_url: &Url) {
+        let client = Client::new();
+        self.cookie_requester.lock().unwrap().send(target_url.clone()).unwrap();
+        let mut req_headers = req.headers.clone();
+        req_headers.remove::<AcceptEncoding>();
+        if let Some(cookies) = self.cookie_receiver.lock().unwrap().recv().unwrap() {
+            req_headers.set(Cookie(cookies));
+        }
+        println!("debug {:?}", &req_headers);
+        let mut proxied_res = client.request(req.method, target_url.clone()).headers(req_headers).send().unwrap();
+        let mut buffer = Vec::new();
+        let _ = proxied_res.read_to_end(&mut buffer);
+        if let Some(&SetCookie(ref cookie_pairs)) = proxied_res.headers.get::<SetCookie>() {
+            self.cookie_sender.lock().unwrap().send((target_url.clone(), cookie_pairs.clone())).unwrap();
+        }
+        {
+            let h: &mut Headers = res.headers_mut();
+            *h = proxied_res.headers.clone();
+            println!("debug {:?}", &h);
+            h.remove::<SetCookie>();
+        }
+        let mut res = res.start().unwrap();
+        println!("debug {}", buffer.len());
+        match fixdom(target_url, str::from_utf8(&buffer).unwrap()) {
+            Some(fixed) => res.write_all(&fixed.as_bytes()).unwrap(),
+            None => res.write_all(&buffer).unwrap(),
+        };
+    }
 }
 
 fn main() {
-    Server::http("127.0.0.1:8080").unwrap().handle(hello).unwrap();
+    let (cookie_sender_tx, cookie_sender_rx) = channel();
+    let (cookie_requester_tx, cookie_requester_rx) = sync_channel(0);
+    let (cookie_receiver_tx, cookie_receiver_rx) = sync_channel(0);
+    let handler = WebypassHandler {
+        cookie_sender: Mutex::new(cookie_sender_tx),
+        cookie_requester: Mutex::new(cookie_requester_tx),
+        cookie_receiver: Mutex::new(cookie_receiver_rx),
+    };
+
+    // {domain: {cookie_name: cookie}}
+    let cookie_jar: HashMap<String, HashMap<String, CookiePair>> = HashMap::new();
+    let cookie_jar_rc = Arc::new(Mutex::new(cookie_jar));
+
+    {
+        let jar_ref = cookie_jar_rc.clone();
+        thread::spawn(move || {
+            while let Ok((target_url, cookiepairs)) = cookie_sender_rx.recv() {
+                let mut jar_handle = jar_ref.lock().unwrap();
+                println!("Recv! {:?} {:?}", &target_url.domain(), &cookiepairs);
+                let cookies = jar_handle.entry(target_url.domain().unwrap().to_owned()).or_insert_with(|| { HashMap::new() });
+                for cookiepair in cookiepairs {
+                    cookies.insert(cookiepair.name.clone(), cookiepair);
+                }
+            }
+        });
+    }
+
+    {
+        let jar_ref = cookie_jar_rc.clone();
+        thread::spawn(move || {
+            while let Ok(target_url) = cookie_requester_rx.recv() {
+                println!("Request! {:?}", &target_url);
+                let jar_handle = jar_ref.lock().unwrap();
+                let maybe_cookies = jar_handle.get(target_url.domain().unwrap());
+                let result = maybe_cookies.map(|cookies| cookies.values().map(|pair| pair.clone()).collect());
+                cookie_receiver_tx.send(result).unwrap();
+            }
+        });
+    }
+
+    Server::http("127.0.0.1:8080").unwrap().handle(handler).unwrap();
 }
 
